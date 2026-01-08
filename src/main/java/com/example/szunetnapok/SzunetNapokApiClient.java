17481321597190273181cf841d7a0064c70e34cc
pb-06 <kkandomail@gmail.com>
2026-01-08 09:39:01 +0100

    Apply API key

--- src/main/java/com/example/szunetnapok/SzunetNapokApiClient.java
+++ src/main/java/com/example/szunetnapok/SzunetNapokApiClient.java
@@ -1,17 +1,25 @@
 package com.example.szunetnapok;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class SzunetNapokApiClient {
-    public static String baseUrlStr = "https://szunetnapok.hu/api/";
+    public static String baseUrlStr = "https://szunetnapok.hu/api";
+
+    public static String getApiKey() throws FileNotFoundException {
+        Scanner fs = new Scanner(new FileReader("szunetnapok-api.key"));
+        return fs.nextLine();
+    }
 
     public static YearHolidays getYear(int year) throws IOException, InterruptedException {
-        String apiKey = ""; // TODO
+        String apiKey = getApiKey();
         String endpoint = baseUrlStr + "/" + apiKey + "/" + year + "/json/";
 
         HttpClient http = HttpClient.newBuilder().build();
